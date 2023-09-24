8d3db51e780eab59b309a0356012e4ff3308725c
Collins-cl <collinsolads@gmail.com>
2023-09-24 17:49:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <Navbar />
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+          <p>dddd</p>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "../Navbar/Navbar.scss";
+import Logo from "../../assets/logo.png";
+import { Link, NavLink } from "react-router-dom";
+import { HiBars3 } from "react-icons/hi2";
+import { FaBars } from "react-icons/fa";
+
+import { LiaTimesSolid } from "react-icons/lia";
+import { BsSearch, BsFillCartFill } from "react-icons/bs";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const openNav = () => setOpen(true);
+  const closeNav = () => setOpen(false);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left-float">
+          <div className="logo">
+            <FaBars onClick={openNav} className="hamburger" />
+
+            <Link to="/">
+              <img src={Logo} width={110} alt="" />
+            </Link>
+          </div>
+
+          <div className={open ? "mobile-nav" : "navlinks"}>
+            <LiaTimesSolid className="close-menu" onClick={closeNav} />
+            <img src={Logo} alt="" />
+
+            <NavLink exact actvieClassName="active" to="/">
+              Home
+            </NavLink>
+
+            <NavLink exact actvieClassName="active" to="/recommendation">
+              Recommendations
+            </NavLink>
+
+            <NavLink>Privacy Policy</NavLink>
+            <NavLink>Terms of service</NavLink>
+            <NavLink>Download on Appstore</NavLink>
+            <NavLink>Log in</NavLink>
+          </div>
+        </div>
+
+        <div className="right-float">
+          <Link to="" className="search">
+            <BsSearch className="icon" />
+          </Link>
+
+          <div className="log-in">
+            <Link>Log In</Link>
+          </div>
+
+          <div className="cart">
+            <BsFillCartFill className="icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
