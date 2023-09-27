58f61f7ffacb14fad2ddfffbe85b32f3d4bebc0e
Collins-cl <collinsolads@gmail.com>
2023-09-27 09:51:23 +0100

    integrated cuisines section

---
+++ src/Components/Cuisines/Cuisine.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "../Cuisines/Cuisine.scss";
+import { NavLink } from "react-router-dom";
+import { CuisineRoute } from "../DummyFiles/CuisineRoute";
+
+const Cuisine = () => {
+  const [itemstoshow, setItemsToShow] = useState(6);
+
+  const totalItems = CuisineRoute.length;
+  return (
+    <div className="cuisine">
+      <div className="wrapper">
+        <div className="head">Choose from cuisines</div>
+
+        <div className="container">
+          {CuisineRoute &&
+            CuisineRoute.slice(0, itemstoshow).map((data, id) => (
+              <NavLink to={`/restaurant/${data.route}`} key={id}>
+                <div className="content">
+                  <div className="image">
+                    <img src={data.img} alt="" />
+                  </div>
+
+                  <p>{data.title}</p>
+                </div>
+              </NavLink>
+            ))}
+        </div>
+
+        {itemstoshow < totalItems && (
+          <div
+            className="see-more"
+            onClick={() => setItemsToShow(itemstoshow + 6)}
+          >
+            Show more cuisines
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cuisine;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../Components/Hero/Hero";
 import Inspo from "../../Components/Inspo/Inspo";
+import Cuisine from "../../Components/Cuisines/Cuisine";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Inspo />
+      <Cuisine />
       <p>dddd</p>
       <p>dddd</p>
       <p>dddd</p>
